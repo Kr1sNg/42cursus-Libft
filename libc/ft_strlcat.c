917e3fe24532e774ea16d61f02b924137f2bbdfe
Tat Hoang Nguyen <tat-nguy@c3r1p4.42perpignan.fr>
2024-11-05 16:01:16 +0100

    ft_strnstr

--- libc/ft_strlcat.c
+++ libc/ft_strlcat.c
@@ -6,11 +6,14 @@
 /*   By: tat-nguy <tat-nguy@student.42perpignan.fr  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 11:43:29 by tat-nguy          #+#    #+#             */
-/*   Updated: 2024/11/05 12:04:17 by tat-nguy         ###   ########.fr       */
+/*   Updated: 2024/11/05 14:35:31 by tat-nguy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-size_t	ft_len(const char *restrict str)
+// return length of (src + dest) or (src + size (if size < dest))
+#include <bsd/string.h>
+
+size_t	ft_len(const char *str)
 {
 	size_t	i;
 
@@ -20,5 +23,38 @@ size_t	ft_len(const char *restrict str)
 	return (i);
 }
 
-size_t	strlcat(char *restrict dst, const char *restrict src, size_t dstsize)
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
+	size_t	i;
+	size_t	d_len;
+	size_t	s_len;
+
+	s_len = ft_len(src);
+	d_len = ft_len(dst);
+	if (size <= d_len)
+		return (size + s_len);
+	i = 0;
+	while (src[i] != '\0' && (d_len + i < size - 1))
+	{
+		dst[d_len + i] = src[i];
+		i++;
+	}
+	dst[d_len + i] = '\0';
+	return (d_len + s_len);
+}
+
+/*
+#include <stdio.h>
+int	main(void)
+{
+	unsigned int	s = 0; 
+	char	*src = "This is 11.";
+	char	dst1[20] = "This is 9";
+	char	dst2[20] = "This is 9";
+
+	printf("len_dst1: %lu, len_dst2: %lu\n", ft_len(dst1), ft_len(dst2));
+	printf("strlcat: %lu, dst: %s\n", strlcat(dst1, src, s), dst1);
+	printf("ft_strlcat: %lu, dst: %s\n", ft_strlcat(dst2, src, s), dst2);
+	return (0);
+}
+*/
--- libc/ft_strlcpy_mac.c
+++ libc/ft_strlcpy_mac.c
@@ -6,13 +6,13 @@
 /*   By: tat-nguy <tat-nguy@student.42perpignan.fr  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 10:20:04 by tat-nguy          #+#    #+#             */
-/*   Updated: 2024/11/05 14:02:15 by tat-nguy         ###   ########.fr       */
+/*   Updated: 2024/11/05 13:30:17 by tat-nguy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <bsd/string.h>
+#include <string.h>
 
-size_t	ft_len(const char *str)
+size_t	ft_len(const char *restrict str)
 {
 	size_t	i;
 
@@ -22,16 +22,16 @@ size_t	ft_len(const char *str)
 	return (i);
 }
 
-size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+size_t	ft_strlcpy(char *restrict dst, const char *restrict src, size_t dstsize)
 {
 	size_t	i;
 	size_t	len_src;
 
 	i = 0;
 	len_src = ft_len(src);
-	if (size == 0)
+	if (dstsize == 0)
 		return (len_src);
-	while (i < (size - 1) && src[i] != '\0')
+	while (i < (dstsize - 1) && src[i] != '\0')
 	{
 		dst[i] = src[i];
 		i++;
@@ -40,16 +40,16 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 	return (len_src);
 }
 
-/*
+
 #include <stdio.h>
 int	main(void)
 {
 	unsigned int n = 12;
-	char dst1[0];
-	char dst2[0];
+	char dst1[n];
+	char dst2[n];
 	const char *src = "This is 9";
 	printf("strlcpy: %lu, dst: %s\n", strlcpy(dst1, src, n), dst1);
 	printf("ft_strlcpy: %lu, dst: %s\n", ft_strlcpy(dst2, src, n), dst2);
 	return (0);
 }
-*/	
+	
