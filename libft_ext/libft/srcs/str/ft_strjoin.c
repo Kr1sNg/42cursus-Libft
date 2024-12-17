7cd1686fa5b065f0ef87af121c0abb2432a3ce89
Tat Hoang Nguyen <tat-nguy@c2r8p8.42perpignan.fr>
2024-12-17 19:01:56 +0100

    libft-ext

--- libft_ext/libft/srcs/str/ft_strjoin.c
+++ libft_ext/libft/srcs/str/ft_strjoin.c
@@ -24,21 +24,21 @@
 
 #include "../../includes/libft.h"
 
-static char	*ft_strcat(char *dest, const char *src)
-{
-	size_t	i;
-	size_t	d_len;
+	// static char	*ft_strcat(char *dest, const char *src)
+	// {
+	// 	size_t	i;
+	// 	size_t	d_len;
 
-	d_len = ft_strlen(dest);
-	i = 0;
-	while (src[i] != '\0')
-	{
-		dest[d_len + i] = src[i];
-		i++;
-	}
-	dest[d_len + i] = '\0';
-	return (dest);
-}
+	// 	d_len = ft_strlen(dest);
+	// 	i = 0;
+	// 	while (src[i] != '\0')
+	// 	{
+	// 		dest[d_len + i] = src[i];
+	// 		i++;
+	// 	}
+	// 	dest[d_len + i] = '\0';
+	// 	return (dest);
+	// }
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
